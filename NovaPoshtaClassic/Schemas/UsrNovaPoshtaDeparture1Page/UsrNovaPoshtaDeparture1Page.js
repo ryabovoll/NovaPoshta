ce3eda8b33ebd1339e868344c10a3e66cfbb6d06
Oleksandr Riabovol <ryabovol.97@gmail.com>
2024-02-26 00:18:50 +0200

    Добавил справочники и поля для формирования запроса на создание накладной

--- NovaPoshtaClassic/Schemas/UsrNovaPoshtaDeparture1Page/UsrNovaPoshtaDeparture1Page.js
+++ NovaPoshtaClassic/Schemas/UsrNovaPoshtaDeparture1Page/UsrNovaPoshtaDeparture1Page.js
@@ -117,6 +117,562 @@ define("UsrNovaPoshtaDeparture1Page", [], function() {
 		diff: /**SCHEMA_DIFF*/[
 			{
 				"operation": "insert",
+				"name": "STRINGaaeec792-10fa-4ed5-8def-952ef6dcef5b",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrPayerType",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "STRINGed4aae6d-89fd-4372-9dfe-31a0ea2aa49f",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrPaymentMethod",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "DATETIME2f01b3f4-4e30-4bdb-9c9e-17a2c2fb7ed7",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 2,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrDateTime",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "STRINGf55375bd-2f38-4b9b-90e8-191acbfb7300",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 3,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrCargoType",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "STRINGd1c80aa7-9d9f-483a-a5fd-ad592f4dc0d8",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 4,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrWeight",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 4
+			},
+			{
+				"operation": "insert",
+				"name": "STRING5e0eb443-277c-4a02-87a7-1d9562658916",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 5,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrServiceType",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 5
+			},
+			{
+				"operation": "insert",
+				"name": "STRINGca8eee4f-c304-40ee-b9b9-e0a70da24b1d",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 6,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrSeatsAmount",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 6
+			},
+			{
+				"operation": "insert",
+				"name": "STRING1a744842-5b46-4c72-adf2-eec0d5c4839a",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 7,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrDescription",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 7
+			},
+			{
+				"operation": "insert",
+				"name": "INTEGERb28af9ab-1e3c-4c68-88da-9343f4761ba9",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 8,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrInteger1",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 8
+			},
+			{
+				"operation": "insert",
+				"name": "STRING95053a79-da02-4ebd-ba33-6ae8c37735dd",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 9,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrRecipientsPhone",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 9
+			},
+			{
+				"operation": "insert",
+				"name": "STRING8cd81c90-6e7d-4c35-93d7-a6ef622ae2ac",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 10,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrRecipientCityName",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 10
+			},
+			{
+				"operation": "insert",
+				"name": "STRING269d61bb-8845-4c30-aa5e-cdf436907fe1",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 11,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrRecipientArea",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 11
+			},
+			{
+				"operation": "insert",
+				"name": "STRING4dd4c9ad-2156-4c21-b6bd-8d6180150534",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 12,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrRecipientAddressName",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 12
+			},
+			{
+				"operation": "insert",
+				"name": "STRINGe9b8929f-51fb-4852-8a0b-b2c914985e65",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 13,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrRecipientHouse",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 13
+			},
+			{
+				"operation": "insert",
+				"name": "STRINGa0664819-cee3-46b8-aa99-d7c27b300b42",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 14,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrRecipientFlat",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 14
+			},
+			{
+				"operation": "insert",
+				"name": "STRINGceef1188-0e38-4029-b392-0bb8015bb23b",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 15,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrRecipientName",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 15
+			},
+			{
+				"operation": "insert",
+				"name": "STRING02b2e851-e066-4784-9bb0-4c3766746722",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 16,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrRecipientType",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 16
+			},
+			{
+				"operation": "insert",
+				"name": "STRINGe8c3d74a-36d9-40bd-8482-2a946705e332",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 17,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrSettlementType",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 17
+			},
+			{
+				"operation": "insert",
+				"name": "STRING6a0f777b-9a3c-4a75-9a0d-2d0dfa91c04b",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 18,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrEDRPOU",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 18
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUPd4a447ca-1385-4d60-bcd0-2e351fcd866a",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrPayerTypeList",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUP2c83cdf1-d430-4caa-9c70-6351b72fda89",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrPaymentMethodList",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUP756056e3-29df-4264-b0c7-a90badf2cb42",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 3,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrCargoTypeList",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUPd390d756-079f-408e-926b-253a5ddd7568",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 4,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrWeightList",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "UsrPostomat71cf60a0-b017-48cc-b0f0-474f66be55dd",
+				"values": {
+					"layout": {
+						"colSpan": 6,
+						"rowSpan": 1,
+						"column": 6,
+						"row": 5,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrPostomat"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 4
+			},
+			{
+				"operation": "insert",
+				"name": "UsrDepartmenta809b3c7-dd70-4e67-9484-ce7185a152f7",
+				"values": {
+					"layout": {
+						"colSpan": 6,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 5,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrDepartment"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 5
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUP02c54105-75f9-4b67-b660-7e7ffe6a1f16",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 6,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrSeatsAmountList",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 6
+			},
+			{
+				"operation": "insert",
+				"name": "UsrCargoTypeLista9ac7dbf-39d8-46fc-adf8-01317725f70d",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 7,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrCargoTypeList"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 7
+			},
+			{
+				"operation": "insert",
+				"name": "UsrDepartment98a03269-caf7-4b07-85ed-97df3be01b42",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 10,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrDepartment"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 8
+			},
+			{
+				"operation": "insert",
+				"name": "UsrDepartmentfc1eb336-0f27-49ad-b513-5e27391a9f3b",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 11,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrDepartment"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 9
+			},
+			{
+				"operation": "insert",
+				"name": "UsrDepartmenta55817eb-d25d-4844-b9ca-cc589cce363f",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 12,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrDepartment"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 10
+			},
+			{
+				"operation": "insert",
+				"name": "UsrDepartment07fa4e1b-88ea-4a3e-8541-f0c2fad563dd",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 17,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrDepartment"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 11
+			},
+			{
+				"operation": "insert",
 				"name": "Tab9959d105TabLabel",
 				"values": {
 					"caption": {
