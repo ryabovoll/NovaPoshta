14d9c3ebe9c4d2b7ccb427ff37c786e6cef31878
Oleksandr Riabovol <ryabovol.97@gmail.com>
2024-09-20 11:56:02 +0300

    tets commit

--- NovaPoshtaApi/Schemas/ARVCounterpartyNP14efca0eSection/ARVCounterpartyNP14efca0eSection.js
+++
@@ -1,8 +0,0 @@
-define("ARVCounterpartyNP14efca0eSection", [], function() {
-	return {
-		entitySchemaName: "ARVCounterpartyNP",
-		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
-	};
-});
--- NovaPoshtaApi/Schemas/ARVNovaPoshtaDeparturee035dae5Section/ARVNovaPoshtaDeparturee035dae5Section.js
+++
@@ -1,8 +0,0 @@
-define("ARVNovaPoshtaDeparturee035dae5Section", [], function() {
-	return {
-		entitySchemaName: "ARVNovaPoshtaDeparture",
-		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
-	};
-});
